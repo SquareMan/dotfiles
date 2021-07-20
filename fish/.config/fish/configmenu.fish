105f741b74bcd7e5fcd6a14e5a80a21775a51e1e
Steven Casper <sebastiancasper3@gmail.com>
2021-07-20 18:01:21 -0400

    configmenu: do not echo newline

--- fish/.config/fish/configmenu.fish
+++ fish/.config/fish/configmenu.fish
@@ -1,6 +1,6 @@
 #!/bin/fish
 
-set -l CHOICE (echo "$configs" | rofi -sep ' ' -dmenu -P "Config File")
+set -l CHOICE (echo -n "$configs" | rofi -sep ' ' -dmenu -P "Config File")
 
 if test -f "$CHOICE"
     $TERMINAL $EDITOR $CHOICE
