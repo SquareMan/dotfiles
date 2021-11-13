04c9574488d0c3b0a846fcf874a80ad4ed4de55b
Steven Casper <sebastiancasper3@gmail.com>
2021-11-12 20:58:37 -0500

    fish: add suspend to powermenu

--- fish/.config/fish/powermenu.fish
+++ fish/.config/fish/powermenu.fish
@@ -5,7 +5,7 @@ set OPTIONS "Shutdown\nRestart"
 if test (cat /proc/cmdline | tr ' ' \n | grep resume=)
     set OPTIONS "$OPTIONS"\\nHibernate
 end
-set OPTIONS "$OPTIONS"\\nLogout
+set OPTIONS "$OPTIONS"\\nSuspend\\nLogout
 
 set -l CHOICE (echo -e $OPTIONS | rofi -dmenu -p Power)
 if test -z "$CHOICE"
@@ -19,6 +19,8 @@ else if test "$CHOICE" = Restart
     reboot
 else if test "$CHOICE" = Hibernate
     systemctl hibernate
+else if test "$CHOICE" = Suspend
+    systemctl suspend
 else if test "$CHOICE" = Logout
     i3-msg exit
 end
